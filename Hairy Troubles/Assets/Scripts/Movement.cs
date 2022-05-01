5bca63f760c54ed08fbcb30a9f5b2c9680cfc7e3
Lautaro Coccia <lautaroauditore@gmail.com>
2022-05-01 19:34:41 -0300

    Add jump

--- Hairy Troubles/Assets/Scripts/Movement.cs
+++ Hairy Troubles/Assets/Scripts/Movement.cs
@@ -7,12 +7,14 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float jumpfoce;
     [SerializeField] float cos;
     [SerializeField] Rigidbody rb;
     float hor;
     float ver;
 
     Vector3 movementDirection;
+    bool canJump = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +29,11 @@ public class Movement : MonoBehaviour
         movementDirection = new Vector3(hor, 0, ver);
         movementDirection.Normalize();
 
-        //if (movementDirection != Vector3.zero)
-        //{
-        //    Quaternion rotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-        //}
-
+        if(Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(new Vector3(0, jumpfoce, 0), ForceMode.Impulse);
+            canJump = false;
+        }
     }
     private void FixedUpdate()
     {
@@ -43,5 +44,10 @@ public class Movement : MonoBehaviour
             Quaternion rotation = Quaternion.LookRotation(movementDirection , Vector3.up);
             rb.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed);
         }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
     }
 }
