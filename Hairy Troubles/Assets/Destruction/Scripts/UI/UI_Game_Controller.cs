7104c8f5d7b17944e6b44e0624566efe6e4a4147
Tomas <Aquista35@gmail.com>
2022-06-13 22:40:05 -0300

    Added UI gameplay

--- Hairy Troubles/Assets/Destruction/Scripts/UI/UI_Game_Controller.cs
+++ Hairy Troubles/Assets/Destruction/Scripts/UI/UI_Game_Controller.cs
@@ -5,11 +5,10 @@ using TMPro;
 
 public class UI_Game_Controller : MonoBehaviour
 {
+    #region EXPOSED_FIELD
     [Header("Stars")]
     [Header("--- STARS ---")]
-    [SerializeField] private GameObject starsBackground;
     [SerializeField] private GameObject[] enableStars;
-
 
     [Header("--- PROGRESS ---")]
 
@@ -17,29 +16,49 @@ public class UI_Game_Controller : MonoBehaviour
     [Header("Percentage")]
     [SerializeField] private Image percentageBar;
     [SerializeField] private TextMeshProUGUI percentageText;
-    private float currentPercentage = 0f;
-    private float maxPercentage = 100f;
 
     [Space(15)]
     [Header("Timer")]
     [SerializeField] private TextMeshProUGUI timerText;
 
-    [Header("Menu Layer")]
-    [SerializeField] private GameObject menuLayer;
+    [Header("Screens")]
+    [SerializeField] private GameObject viewEnd;
+    [SerializeField] private GameObject viewPause;
     [SerializeField] private GameObject timerObj;
 
-    // ----------------
+    [SerializeField] private string sceneMenu = "MainMenu";
 
+    [Header("Transitioner")]
+    [SerializeField] private SceneTransition transitioner;
+    #endregion
+
+    #region PRIVATE_FIELD
+    private float currentPercentage = 0f;
+    private float maxPercentage = 100f;
+
+    private bool pauseState = true;
+    #endregion
+
+    #region UNITY_CALLS
     private void Awake()
     {
         for (int i = 0; i < enableStars.Length; i++)
         {
             enableStars[i].SetActive(false);
         }
+        DisablePause();
     }
 
-    // ----------------
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            DisablePause();
+        }
+    }
+    #endregion
 
+    #region PUBLIC_CALLS
     public void ActivateStar(int i)
     {
         enableStars[i].SetActive(true);
@@ -52,18 +71,30 @@ public class UI_Game_Controller : MonoBehaviour
 
     public void ActivateMenu(bool state)
     {
-        starsBackground.SetActive(state);
-        menuLayer.SetActive(state);
+        viewEnd.SetActive(state);
         progressBar.SetActive(!state);
         timerObj.SetActive(!state);
     }
 
-    public void RestartScene()
+    public void OnRestartScene(bool isPause)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+        if(isPause) DisablePause();
 
-    // ------------------------
+        transitioner.ChangeAnimation(1, ()=> 
+        { 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        });
+    }
+    
+    public void OnGoToScene(bool isPause)
+    {
+        if(isPause) DisablePause();
+
+        transitioner.ChangeAnimation(1, () =>
+        {
+            SceneManager.LoadScene(sceneMenu);
+        });
+    }
 
     public void SetMaximumProgress(float value)
     {
@@ -77,4 +108,23 @@ public class UI_Game_Controller : MonoBehaviour
 
         percentageText.text = "%" + (percentageBar.fillAmount * 100).ToString("0");
     }
+    #endregion
+
+    #region PRIVATE_CALLS
+    private void DisablePause()
+    {
+        pauseState = !pauseState;
+
+        if (!pauseState)
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+        }
+
+        viewPause.SetActive(pauseState);
+    }
+    #endregion
 }
