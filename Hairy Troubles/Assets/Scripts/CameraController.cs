7fd9cd0ed1f5c7b8c8fa02baf018f0a01c7e02f0
Lautaro Coccia <lautaroauditore@gmail.com>
2022-06-18 16:40:17 -0300

    Fix player controller

--- Hairy Troubles/Assets/Scripts/CameraController.cs
+++ Hairy Troubles/Assets/Scripts/CameraController.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 public class CameraController : MonoBehaviour
 {
-    public Transform player;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
-    void FixedUpdate()
+    [SerializeField] Transform player;
+    [SerializeField] float smoothSpeed = 0.125f;
+    [SerializeField] Vector3 positionOffset;
+    [SerializeField] Vector3 rotationOffset;
+    [SerializeField] bool lookAt;
+    void LateUpdate()
     {
-        Vector3 desiredPosition = player.position + offset;
+        Vector3 desiredPosition = player.position + positionOffset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
-        transform.LookAt(player);
+        if(lookAt)
+        {
+            transform.LookAt(player);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(rotationOffset.x, rotationOffset.y, rotationOffset.z);
+        }
     }
 }
  
