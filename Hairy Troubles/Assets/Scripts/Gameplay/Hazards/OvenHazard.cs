317b038aa897534d21f2945be65c573eb007ae81
Tomas <Aquista35@gmail.com>
2022-10-28 16:37:33 -0300

    add mouse hazard

--- Hairy Troubles/Assets/Scripts/Gameplay/Hazards/OvenHazard.cs
+++ Hairy Troubles/Assets/Scripts/Gameplay/Hazards/OvenHazard.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class OvenHazard : Hazard
 {
+    #region EXPOSED_FIELD
+    [SerializeField] [Range(0,10)] private float activeTime = 3f;
+    [SerializeField] [Range(0,10)] private float timeToActivate = 3f;
+
+    [SerializeField] private OvenFire[] ovenParticles;
+    #endregion
+
+    #region PRIVATE_FIELD
+    private float ovenTime = 0f;
+    private bool active = false;
+    #endregion
 
     #region UNITY_CALLS
+    private void Start()
+    {
+        for (int i = 0; i < ovenParticles.Length; i++)
+        {
+            ovenParticles[i].Init();
+        }
+    }
+
     protected override void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out StatusEffects player))
         {
             player.BurningState(timerEffect);
+        }
+    }
+
+    private void Update()
+    {
+        ovenTime += Time.deltaTime;
+
+        if (active)
+        {
+            if (ovenTime > activeTime)
+            {
+                active = false;
+                EnableCollider(active);
+
+                SwapParticles(active);
+
+                ovenTime = 0;
+            }
+        }
+        else
+        {
+            if (ovenTime > timeToActivate)
+            {
+                active = true;
+                EnableCollider(active);
+
+                SwapParticles(active);
+
+                ovenTime = 0;
+            }
         }
     }
     #endregion
@@ -19,6 +68,16 @@ public class OvenHazard : Hazard
     protected override void TriggerEvent()
     {
 
+    }
+    #endregion
+
+    #region PRIVATE_CALLS
+    private void SwapParticles(bool state)
+    {
+        for (int i = 0; i < ovenParticles.Length; i++)
+        {
+            ovenParticles[i].SwapActiveParticle(state);
+        }
     }
     #endregion
 }
